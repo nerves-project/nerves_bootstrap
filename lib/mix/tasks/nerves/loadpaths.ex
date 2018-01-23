de254b4319f95733eaaac6d3e231c82f3daf19d7
Justin Schneck <jschneck@mac.com>
2018-01-23 10:40:32 -0500

    clean up logic which clears deps cache

--- lib/mix/tasks/nerves/loadpaths.ex
+++ lib/mix/tasks/nerves/loadpaths.ex
@@ -11,7 +11,7 @@ defmodule Mix.Tasks.Nerves.Loadpaths do
           try do
             Mix.Task.run("nerves.env", [])
             Nerves.Env.bootstrap()
-            reload_cache()
+            clear_deps_cache()
             env_info()
           rescue
             e ->
@@ -40,24 +40,15 @@ defmodule Mix.Tasks.Nerves.Loadpaths do
     """)
   end
 
-  defp reload_cache do
-    # Fetch the app names from the cached dependencies
-    #  Then consult the Mix.ProjectStack to get the 
-    #  Mixfile module and purge it from the code server
-    # This will force the reevaluation of the dependency
-    #  ensuring that it is evaluated agains the bootstrapped env
-    Mix.Dep.cached()
-    |> Enum.map(&Map.get(&1, :app))
-    |> Enum.uniq
-    |> Enum.map(&Mix.ProjectStack.read_cache({:app, &1}))
-    |> Enum.each(fn({mod, _file}) ->  
-      :code.purge(mod)
-      :code.delete(mod)
-    end)
-
-    # Clear the Mix.ProjectStack cache
-    Mix.ProjectStack.clear_cache()
-
-    :ok
+  defp clear_deps_cache() do
+    if :erlang.function_exported(Mix.Project, :clear_deps_cache, 0) do
+      apply(Mix.Project, :clear_deps_cache, [])
+    else
+      if project = Mix.Project.get() do
+        key = {:cached_deps, Mix.env(), project}
+        Agent.cast(Mix.ProjectStack, &%{&1 | cache: Map.delete(&1.cache, key)})
+      end
+    end
   end
+
 end
