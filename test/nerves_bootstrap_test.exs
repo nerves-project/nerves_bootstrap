8c0be50a0e69da9e25de14124958c67aaacf5b3a
Justin Schneck <jschneck@mac.com>
2018-02-02 09:17:49 -0500

    update tests

--- test/nerves_bootstrap_test.exs
+++ test/nerves_bootstrap_test.exs
@@ -4,28 +4,33 @@ defmodule Nerves.BootstrapTest do
   test "aliases are injected properly" do
     deps_loadpaths = ["nerves.loadpaths", "deps.loadpaths"]
     deps_get = ["deps.get", "nerves.deps.get"]
+    deps_update = ["deps.update", "nerves.deps.get"]
  
     aliases = Nerves.Bootstrap.add_aliases([])
     assert Keyword.get(aliases, :"deps.loadpaths") == deps_loadpaths
     assert Keyword.get(aliases, :"deps.get") == deps_get
+    assert Keyword.get(aliases, :"deps.update") == deps_update
   end
 
   test "custom aliases are maintained" do
     custom_aliases = [
       "deps.loadpaths": ["custom", "deps.loadpaths"],
       "deps.get": ["deps.get", "custom"],
+      "deps.update": ["deps.update", "custom"],
       "custom": ["custom"]
     ]
     aliases = Nerves.Bootstrap.add_aliases(custom_aliases)
 
     assert Keyword.get(aliases, :"deps.loadpaths") == ["nerves.loadpaths", "custom", "deps.loadpaths"]
     assert Keyword.get(aliases, :"deps.get") == ["deps.get", "custom", "nerves.deps.get"]
+    assert Keyword.get(aliases, :"deps.update") == ["deps.update", "custom", "nerves.deps.get"]
     assert Keyword.get(aliases, :"custom") == ["custom"]
   end
 
   test "aliases are dropped if they already exist" do
     deps_loadpaths = ["nerves.loadpaths", "deps.loadpaths"]
     deps_get = ["deps.get", "nerves.deps.get"]
+    deps_update = ["deps.update", "nerves.deps.get"]
  
     nerves_aliases = [
       "deps.loadpaths": deps_loadpaths
@@ -34,6 +39,7 @@ defmodule Nerves.BootstrapTest do
     aliases = Nerves.Bootstrap.add_aliases(nerves_aliases)
     assert Keyword.get(aliases, :"deps.loadpaths") == deps_loadpaths
     assert Keyword.get(aliases, :"deps.get") == deps_get
+    assert Keyword.get(aliases, :"deps.update") == deps_update
   end
 
   test "raise if deps.get alias is missing" do
