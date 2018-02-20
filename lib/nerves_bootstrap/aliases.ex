a71effa3c6c9678a91fa4fb5006cf1c4e956f5a0
Justin Schneck <jschneck@mac.com>
2018-02-20 15:47:15 -0500

    warn when bootstrap was started but the target aliases were not added.

--- lib/nerves_bootstrap/aliases.ex
+++ lib/nerves_bootstrap/aliases.ex
@@ -22,8 +22,8 @@ defmodule Nerves.Bootstrap.Aliases do
     update_in(config, [:aliases], &add_host_aliases(&1))
   end
 
-  def target_config(config, nil), do: config
-  def target_config(config, "host"), do: config
+  def target_config(config, nil), do: host_only(config)
+  def target_config(config, "host"), do: host_only(config)
 
   def target_config(config, target) do
     Mix.shell().info([
@@ -92,5 +92,18 @@ defmodule Nerves.Bootstrap.Aliases do
 
   defp drop(aliases, a) do
     Enum.reject(aliases, &(&1 === a))
+  end
+
+  defp host_only(config) do
+    Mix.shell().info([
+      IO.ANSI.yellow(),
+      """
+      Nerves Environment not loaded.
+        MIX_TARGET is unset
+      """,
+      IO.ANSI.reset()
+    ])
+
+    config
   end
 end
