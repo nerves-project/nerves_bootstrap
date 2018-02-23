735d17000060d380bd00c769a59d84a1ed51cdab
Justin Schneck <jschneck@mac.com>
2018-02-23 10:26:57 -0500

    rework check for update logic to parse and sort based on version and not release order

--- lib/nerves_bootstrap.ex
+++ lib/nerves_bootstrap.ex
@@ -18,6 +18,11 @@ defmodule Nerves.Bootstrap do
   def version, do: @version
 
   @doc """
+  Add the required Nerves bootstrap aliases to the existing ones
+  """
+  defdelegate add_aliases(aliases), to: Nerves.Bootstrap.Aliases
+
+  @doc """
   Check the nerves_bootstrap updates from hex
   """
   def check_for_update() do
@@ -25,34 +30,76 @@ defmodule Nerves.Bootstrap do
       Hex.start()
       {:ok, {200, resp, _}} = Hex.API.Package.get("hexpm", "nerves_bootstrap")
 
-      latest_rel =
-        resp
-        |> Map.get("releases")
-        |> List.first()
-
-      latest_vsn =
-        Map.get(latest_rel, "version")
-        |> Version.parse!()
-
-      current_vsn =
+      current_version =
         Nerves.Bootstrap.version()
         |> Version.parse!()
 
-      if Version.compare(current_vsn, latest_vsn) == :lt do
-        Mix.shell().info([
-          IO.ANSI.yellow(),
-          "A new version of Nerves bootstrap is available(#{current_vsn} < #{latest_vsn}), " <>
-            "please update with `mix local.nerves`",
-          IO.ANSI.reset()
-        ])
+      release_versions =
+        resp
+        |> Map.get("releases")
+        |> Enum.map(&Map.get(&1, "version"))
+        |> Enum.map(&Version.parse!/1)
+
+      case check_for_update(release_versions, current_version) do
+        nil ->
+          :noop
+
+        latest_version ->
+          render_update_message(current_version, latest_version)
       end
     rescue
       _e -> :noop
     end
   end
 
-  @doc """
-  Add the required Nerves bootstrap aliases to the existing ones
-  """
-  defdelegate add_aliases(aliases), to: Nerves.Bootstrap.Aliases
+  def check_for_update(releases, current_version) do
+    releases
+    |> filter_pre_release(current_version)
+    |> Enum.filter(&(Version.compare(&1, current_version) == :gt))
+    |> Enum.sort(&(Version.compare(&1, &2) == :gt))
+    |> List.first()
+  end
+
+  def render_update_message(current_version, %{pre: pre} = latest_version) do
+    message =
+      "A new version of Nerves bootstrap is available(#{current_version} < #{latest_version}), " <>
+        if pre == [] do
+          """
+          You can update by running
+            
+            mix local.nerves
+          """
+        else
+          """
+          You can update by running
+            
+            mix archive.install hex nerves_bootstrap #{latest_version}
+          """
+        end
+
+    Mix.shell().info([
+      IO.ANSI.yellow(),
+      message,
+      IO.ANSI.reset()
+    ])
+  end
+
+  defp filter_pre_release(releases, %{pre: []}) do
+    releases
+    |> Enum.filter(&(Map.get(&1, :pre) == []))
+  end
+
+  defp filter_pre_release(releases, %{major: major, minor: minor, patch: patch}) do
+    releases
+    |> Enum.filter(fn
+      %{pre: []} ->
+        true
+
+      %{major: ^major, minor: ^minor, patch: ^patch} ->
+        true
+
+      _ ->
+        false
+    end)
+  end
 end
