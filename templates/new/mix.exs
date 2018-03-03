a36d1e43f761790ff40ee276083d83fccf22479b
Frank Hunleth <fhunleth@troodon-software.com>
2018-03-03 16:08:55 -0500

    Bump nerves_system dep so that it compiles
    Without this, hex will pull down a pre-1.0.0 version which will fail to
    compile due to nerves dependencies not matching up.

--- templates/new/mix.exs
+++ templates/new/mix.exs
@@ -62,7 +62,7 @@ defmodule <%= app_module %>.MixProject do
   end
 
 <%= for target <- targets do %>
-  defp system("<%= target %>"), do: [{:<%= "nerves_system_#{target}" %>, ">= 0.0.0", runtime: false}]
+  defp system("<%= target %>"), do: [{:<%= "nerves_system_#{target}" %>, "~> 1.0-rc", runtime: false}]
 <% end %>
   defp system(target), do: Mix.raise("Unknown MIX_TARGET: #{target}")
 end
