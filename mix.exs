115e6bc775700b3b539dd0b614a7f0385f094fb4
Frank Hunleth <fhunleth@troodon-software.com>
2018-03-16 11:23:08 -0400

    v1.0.0-rc.2 release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nerves.Bootstrap.Mixfile do
   def project do
     [
       app: :nerves_bootstrap,
-      version: "1.0.0-rc.1",
+      version: "1.0.0-rc.2",
       elixir: "~> 1.4",
       aliases: aliases(),
       xref: [exclude: [Nerves.Env, Nerves.Artifact]],
