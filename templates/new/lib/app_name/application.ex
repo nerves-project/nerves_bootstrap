723283d91594ddf35bb04c200192f08ce08e8fc2
Justin Schneck <jschneck@mac.com>
2018-04-25 14:49:41 -0700

    conditionally define the application supervisor's children instead of application/0

--- templates/new/lib/app_name/application.ex
+++ templates/new/lib/app_name/application.ex
@@ -3,18 +3,29 @@ defmodule <%= app_module %>.Application do
   # for more information on OTP Applications
   @moduledoc false
 
+  @target Mix.Project.config()[:target]
+
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
-    children = [
-      # Starts a worker by calling: <%= app_module %>.Worker.start_link(arg)
-      # {<%= app_module %>.Worker, arg},
-    ]
-
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: <%= app_module %>.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link(children(@target), opts)
+  end
+
+  # List all child processes to be supervised
+  def children("host") do
+    [
+      # Starts a worker by calling: <%= app_module %>.Worker.start_link(arg)
+      # {<%= app_module %>.Worker, arg},
+    ]
+  end
+
+  def children(_target) do
+    [
+      # Starts a worker by calling: <%= app_module %>.Worker.start_link(arg)
+      # {<%= app_module %>.Worker, arg},
+    ]
   end
 end
