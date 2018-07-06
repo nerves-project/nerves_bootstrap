42ae49b52cb2087636bd3f44b5381a3ac0b34ebd
Justin Schneck <jschneck@mac.com>
2018-07-06 17:54:53 -0400

    Configure prod to use the default shoehorn handler

--- templates/new/config/prod.exs
+++ templates/new/config/prod.exs
@@ -1,1 +1,7 @@
 use Mix.Config
+
+# Configure shoehorn to use the default handler in prod.
+# The default handler will call `:erlang.halt()` when an 
+# OTP application exits.
+config :shoehorn,
+  handler: Shoehorn.Handler.Default
