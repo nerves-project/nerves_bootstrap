60485e210971a5b2ffb7e766a7d577d29b8d70d1
Justin Schneck <jschneck@mac.com>
2018-08-21 07:34:43 -0400

    v1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nerves.Bootstrap.Mixfile do
   def project do
     [
       app: :nerves_bootstrap,
-      version: "1.1.0-dev",
+      version: "1.1.0",
       elixir: "~> 1.6",
       aliases: aliases(),
       xref: [exclude: [Nerves.Env, Nerves.Artifact]],
