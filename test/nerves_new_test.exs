89bacb68b91d8f46bbb4f2814daeec93fefa2930
Justin Schneck <jschneck@mac.com>
2019-01-26 20:02:22 -0500

    Bump system minimum versions from 1.5 to 1.6

--- test/nerves_new_test.exs
+++ test/nerves_new_test.exs
@@ -21,12 +21,12 @@ defmodule Nerves.NewTest do
 
       assert_file("#{@app_name}/mix.exs", fn file ->
         assert file =~ "app: :#{@app_name}"
-        assert file =~ "{:nerves_system_rpi, \"~> 1.5\", runtime: false, targets: :rpi"
-        assert file =~ "{:nerves_system_rpi0, \"~> 1.5\", runtime: false, targets: :rpi0"
-        assert file =~ "{:nerves_system_rpi2, \"~> 1.5\", runtime: false, targets: :rpi2"
-        assert file =~ "{:nerves_system_rpi3, \"~> 1.5\", runtime: false, targets: :rpi3"
+        assert file =~ "{:nerves_system_rpi, \"~> 1.6\", runtime: false, targets: :rpi"
+        assert file =~ "{:nerves_system_rpi0, \"~> 1.6\", runtime: false, targets: :rpi0"
+        assert file =~ "{:nerves_system_rpi2, \"~> 1.6\", runtime: false, targets: :rpi2"
+        assert file =~ "{:nerves_system_rpi3, \"~> 1.6\", runtime: false, targets: :rpi3"
         assert file =~ "{:nerves_system_bbb, \"~> 2.0\", runtime: false, targets: :bbb"
-        assert file =~ "{:nerves_system_x86_64, \"~> 1.5\", runtime: false, targets: :x86_64"
+        assert file =~ "{:nerves_system_x86_64, \"~> 1.6\", runtime: false, targets: :x86_64"
       end)
     end)
   end
@@ -39,8 +39,8 @@ defmodule Nerves.NewTest do
 
       assert_file("#{@app_name}/mix.exs", fn file ->
         assert file =~ "app: :#{@app_name}"
-        assert file =~ "{:nerves_system_rpi, \"~> 1.5\", runtime: false, targets: :rpi"
-        refute file =~ "{:nerves_system_rpi0, \"~> 1.5\", runtime: false, targets: :rpi0"
+        assert file =~ "{:nerves_system_rpi, \"~> 1.6\", runtime: false, targets: :rpi"
+        refute file =~ "{:nerves_system_rpi0, \"~> 1.6\", runtime: false, targets: :rpi0"
       end)
     end)
   end
@@ -53,9 +53,9 @@ defmodule Nerves.NewTest do
 
       assert_file("#{@app_name}/mix.exs", fn file ->
         assert file =~ "app: :#{@app_name}"
-        assert file =~ "{:nerves_system_rpi, \"~> 1.5\", runtime: false, targets: :rpi"
-        assert file =~ "{:nerves_system_rpi3, \"~> 1.5\", runtime: false, targets: :rpi3"
-        refute file =~ "{:nerves_system_rpi0, \"~> 1.5\", runtime: false, targets: :rpi0"
+        assert file =~ "{:nerves_system_rpi, \"~> 1.6\", runtime: false, targets: :rpi"
+        assert file =~ "{:nerves_system_rpi3, \"~> 1.6\", runtime: false, targets: :rpi3"
+        refute file =~ "{:nerves_system_rpi0, \"~> 1.6\", runtime: false, targets: :rpi0"
       end)
     end)
   end
