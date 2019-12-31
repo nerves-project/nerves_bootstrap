815379bf46e0916d5b13aacfde608212aabf4e22
Frank Hunleth <fhunleth@troodon-software.com>
2019-12-31 13:56:13 -0600

    Remove trailing whitespace

--- templates/new/config/target.exs
+++ templates/new/config/target.exs
@@ -82,7 +82,7 @@ config :mdns_lite,
       port: 4369
     }
   ]<% end %>
-  
+
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 # Uncomment to use target specific configurations
