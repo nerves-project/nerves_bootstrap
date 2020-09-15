7e5d23f30a2c8c3998c8d43930ede972634db6b3
Frank Hunleth <fhunleth@troodon-software.com>
2020-09-15 19:02:06 -0400

    Add missing :hex and :eex dependencies
    This fixes warnings caught by Elixir 1.11:
    
    ```
    warning: EEx.eval_string/3 defined in application :eex is used by the current application but the current application does not directly depend on :eex.
    warning: Hex.start/0 defined in application :hex is used by the current application but the current application does not directly depend on :hex.
    ```

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Nerves.Bootstrap.MixProject do
 
   def application do
     [
-      extra_applications: [],
+      extra_applications: [:hex, :eex],
       mod: {Nerves.Bootstrap, []}
     ]
   end
