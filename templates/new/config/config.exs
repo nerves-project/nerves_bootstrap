36e1b791c02ffb69dfaf9bd072d97bd8d73830fa
Frank Hunleth <fhunleth@troodon-software.com>
2022-05-01 14:02:39 -0400

    Remove :"" workaround
    This was required from Elixir versions before Elixir 1.11.3. Now that
    Elixir 1.11 is required, remove it.
    
    Fixes #217

--- templates/new/config/config.exs
+++ templates/new/config/config.exs
@@ -26,7 +26,7 @@ config :nerves, source_date_epoch: "<%= source_date_epoch %>"
 
 config :logger, backends: [RingLogger]
 
-if Mix.target() == :host or Mix.target() == :"" do
+if Mix.target() == :host do
   import_config "host.exs"
 else
   import_config "target.exs"
