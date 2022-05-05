53292e2599149103181ae65c9f898e762dc6d811
Frank Hunleth <fhunleth@troodon-software.com>
2022-05-05 11:00:03 -0600

    Ignore remaining dialyzer warnings

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,15 @@
+# Run `mix dialyzer --format short` for strings
+[
+  {"lib/mix/tasks/nerves/clean.ex:32:unknown_function Function Nerves.Env.packages/0 does not exist."},
+  {"lib/mix/tasks/nerves/clean.ex:48:unknown_function Function Nerves.Env.package/1 does not exist."},
+  {"lib/mix/tasks/nerves/clean.ex:52:unknown_function Function Nerves.Env.clean/1 does not exist."},
+  {"lib/mix/tasks/nerves/env.ex:31:unknown_function Function Nerves.Env.start/0 does not exist."},
+  {"lib/mix/tasks/nerves/env.ex:41:unknown_function Function Nerves.Env.packages/0 does not exist."},
+  {"lib/mix/tasks/nerves/loadpaths.ex:16:unknown_function Function Nerves.Env.bootstrap/0 does not exist."},
+  {"lib/mix/tasks/nerves/precompile.ex:25:unknown_function Function Nerves.Env.packages/0 does not exist."},
+  {"lib/mix/tasks/nerves/precompile.ex:57:unknown_function Function Nerves.Artifact.expand_sites/1 does not exist."},
+  {"lib/mix/tasks/nerves/precompile.ex:58:unknown_function Function Nerves.Artifact.stale?/1 does not exist."},
+  {"lib/mix/tasks/nerves/system.shell.ex:62:unknown_function Function Nerves.Env.system/0 does not exist."},
+  {"lib/nerves_bootstrap.ex:29:unknown_function Function Hex.start/0 does not exist."},
+  {"lib/nerves_bootstrap.ex:30:unknown_function Function Hex.API.Package.get/2 does not exist."}
+]
