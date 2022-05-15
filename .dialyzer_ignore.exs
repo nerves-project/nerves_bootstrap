b1c6ded4f506527632fe419a708696ab0484ada1
Jon Carstens <jjcarstens@me.com>
2022-05-15 13:03:42 -0600

    Move core tooling to `:nerves`
    `Nerves.Bootstrap` and `Nerves` share a lot of code back and forth
    which makes things a little hard to maintain and causes confusion
    when trying to following the mix integration with Nerves.
    
    This changes things to move all the tasks to `:nerves` and only use
    Nerves.Bootstrap to provided the Nerves project generator and do
    the initial loading of `:nerves` into the current Nerves project
    being built.
    
    From there, all of the work and heavy lifting with be done with
    `:nerves` and maintained there
    
    The origianl tasks are left in and moved to the `attic` dir for backwards
    compatibility. If this bootstrap is used with the new Nerves, module
    redefinition warnings are suppresed during compilation so that new
    Nerves can redefine these tasks as needed and it will be transparent
    to the user.

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,15 +1,15 @@
 # Run `mix dialyzer --format short` for strings
 [
-  {"lib/mix/tasks/nerves/clean.ex:32:unknown_function Function Nerves.Env.packages/0 does not exist."},
-  {"lib/mix/tasks/nerves/clean.ex:48:unknown_function Function Nerves.Env.package/1 does not exist."},
-  {"lib/mix/tasks/nerves/clean.ex:52:unknown_function Function Nerves.Env.clean/1 does not exist."},
-  {"lib/mix/tasks/nerves/env.ex:31:unknown_function Function Nerves.Env.start/0 does not exist."},
-  {"lib/mix/tasks/nerves/env.ex:40:unknown_function Function Nerves.Env.packages/0 does not exist."},
-  {"lib/mix/tasks/nerves/loadpaths.ex:16:unknown_function Function Nerves.Env.bootstrap/0 does not exist."},
-  {"lib/mix/tasks/nerves/precompile.ex:25:unknown_function Function Nerves.Env.packages/0 does not exist."},
-  {"lib/mix/tasks/nerves/precompile.ex:57:unknown_function Function Nerves.Artifact.expand_sites/1 does not exist."},
-  {"lib/mix/tasks/nerves/precompile.ex:58:unknown_function Function Nerves.Artifact.stale?/1 does not exist."},
-  {"lib/mix/tasks/nerves/system.shell.ex:62:unknown_function Function Nerves.Env.system/0 does not exist."},
-  {"lib/nerves_bootstrap.ex:29:unknown_function Function Hex.start/0 does not exist."},
-  {"lib/nerves_bootstrap.ex:30:unknown_function Function Hex.API.Package.get/2 does not exist."}
+  {"lib/attic/clean.ex:33:unknown_function Function Nerves.Env.packages/0 does not exist."},
+  {"lib/attic/clean.ex:49:unknown_function Function Nerves.Env.package/1 does not exist."},
+  {"lib/attic/clean.ex:53:unknown_function Function Nerves.Env.clean/1 does not exist."},
+  {"lib/attic/env.ex:29:unknown_function Function Nerves.Env.start/0 does not exist."},
+  {"lib/attic/env.ex:38:unknown_function Function Nerves.Env.packages/0 does not exist."},
+  {"lib/attic/loadpaths.ex:16:unknown_function Function Nerves.Env.bootstrap/0 does not exist."},
+  {"lib/attic/precompile.ex:25:unknown_function Function Nerves.Env.packages/0 does not exist."},
+  {"lib/attic/precompile.ex:57:unknown_function Function Nerves.Artifact.expand_sites/1 does not exist."},
+  {"lib/attic/precompile.ex:58:unknown_function Function Nerves.Artifact.stale?/1 does not exist."},
+  {"lib/attic/system.shell.ex:63:unknown_function Function Nerves.Env.system/0 does not exist."},
+  {"lib/nerves_bootstrap.ex:39:unknown_function Function Hex.start/0 does not exist."},
+  {"lib/nerves_bootstrap.ex:40:unknown_function Function Hex.API.Package.get/2 does not exist."}
 ]
