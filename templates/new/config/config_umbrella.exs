705b8c3b83fc789e8d5164ff8d9316f6aaee27b9
Steven Fuchs <stwf@yahoo.com>
2023-04-16 22:58:46 -0400

    compiles as app

--- templates/new/config/config_umbrella.exs
+++ templates/new/config/config_umbrella.exs
@@ -2,21 +2,21 @@
 # dependencies.
 #
 
-# Enable the Nerves integration with Mix
-Application.start(:nerves_bootstrap)
+if Mix.target() != :host do
+  # Enable the Nerves integration with Mix
+  Application.start(:nerves_bootstrap)
 
-config :<%= app_name %>, target: Mix.target()
+  config :<%= app_name %>, target: Mix.target()
+  # Customize non-Elixir parts of the firmware. See
+  # https://hexdocs.pm/nerves/advanced-configuration.html for details.
 
-# Customize non-Elixir parts of the firmware. See
-# https://hexdocs.pm/nerves/advanced-configuration.html for details.
+  config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 
-config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
+  # Set the SOURCE_DATE_EPOCH date for reproducible builds.
+  # See https://reproducible-builds.org/docs/source-date-epoch/ for more information
 
-# Set the SOURCE_DATE_EPOCH date for reproducible builds.
-# See https://reproducible-builds.org/docs/source-date-epoch/ for more information
-
-config :nerves, source_date_epoch: "<%= source_date_epoch %>"
-
+  config :nerves, source_date_epoch: "<%= source_date_epoch %>"
+end
 
 if Mix.target() == :host do
   import_config "host.exs"
