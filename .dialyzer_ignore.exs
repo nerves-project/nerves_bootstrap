7aca5fc749e93c27b323f665a6033295425360a3
Jon Carstens <jjcarstens@me.com>
2023-09-27 18:06:09 -0600

    Hard requirement for `:nerves >= 1.8`
    The mix tooling was moved to Nerves starting in 1.8. Since we removed the
    duplicated tasks from the attic code, we now need to require this version
    of Nerves for future versions of Nerves bootstrap.
    
    This adds compilation failures and warnings where possible to help people
    through the transition

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,5 @@
 # Run `mix dialyzer --format short` for strings
 [
-  {"lib/nerves_bootstrap.ex:39:unknown_function Function Hex.start/0 does not exist."},
-  {"lib/nerves_bootstrap.ex:40:unknown_function Function Hex.API.Package.get/2 does not exist."}
+  {"lib/nerves_bootstrap.ex:52:unknown_function Function Hex.start/0 does not exist."},
+  {"lib/nerves_bootstrap.ex:53:unknown_function Function Hex.API.Package.get/2 does not exist."}
 ]
