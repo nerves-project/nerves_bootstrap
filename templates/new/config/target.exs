472edb19770b5a4b3326696d35506008cc395a22
Benjamin Milde <benni@kobrakai.de>
2024-02-22 18:17:12 +0100

    Simpify searching ssh key

--- templates/new/config/target.exs
+++ templates/new/config/target.exs
@@ -25,12 +25,9 @@ config :nerves, :erlinit, update_clock: true
 # * See https://hexdocs.pm/ssh_subsystem_fwup/readme.html for firmware updates
 
 keys =
-  [
-    Path.join([System.user_home!(), ".ssh", "id_rsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ecdsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ed25519.pub"])
-  ]
-  |> Enum.filter(&File.exists?/1)
+  System.user_home!()
+  |> Path.join(".ssh/id_{rsa,ecdsa,ed25519}.pub")
+  |> Path.wildcard()
 
 if keys == [],
   do:
