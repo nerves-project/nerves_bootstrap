47dfdd31b768bd0a8d933657b9f29679f1a732ba
Frank Hunleth <fhunleth@troodon-software.com>
2025-01-26 21:08:48 -0500

    Directly save bootstrap version to save some lines

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,5 @@
 # Run `mix dialyzer --format short` for strings
 [
-  {"lib/nerves_bootstrap.ex:41:9:unknown_function Function Hex.start/0 does not exist."},
-  {"lib/nerves_bootstrap.ex:42:45:unknown_function Function Hex.API.Package.get/2 does not exist."}
+  {"lib/nerves_bootstrap.ex:39:9:unknown_function Function Hex.start/0 does not exist."},
+  {"lib/nerves_bootstrap.ex:40:45:unknown_function Function Hex.API.Package.get/2 does not exist."}
 ]
