6e4fac7b530a0b3a7b8e62f9d980d97ef6eb5d0c
Frank Hunleth <fhunleth@troodon-software.com>
2025-10-18 17:48:43 -0400

    Disable Elixir 1.19's parallel compilation for now
    It currently isn't work. This temporarily turns it off when doing Nerves
    builds and prints a warning. Hopefully we'll get this fixed quickly, but
    this will keep things from breaking in the mean time.

--- lib/nerves_bootstrap.ex
+++ lib/nerves_bootstrap.ex
@@ -10,8 +10,30 @@ defmodule Nerves.Bootstrap do
 
   @impl Application
   def start(_type, _args) do
+    if Version.match?(System.version(), ">= 1.19.0") and has_compile_partition_count?() do
+      Mix.shell().error("""
+      Nerves does not support MIX_OS_DEPS_COMPILE_PARTITION_COUNT yet.
+
+      Disabling it for this build. To silence this warning, either unset
+      MIX_OS_DEPS_COMPILE_PARTITION_COUNT or set it to 1 before you build.
+
+      Follow https://github.com/nerves-project/nerves/issues/1093 to track
+      progress.
+      """)
+
+      System.delete_env("MIX_OS_DEPS_COMPILE_PARTITION_COUNT")
+    end
+
     Nerves.Bootstrap.Aliases.init()
     {:ok, self()}
+  end
+
+  defp has_compile_partition_count?() do
+    case System.fetch_env("MIX_OS_DEPS_COMPILE_PARTITION_COUNT") do
+      :error -> false
+      {:ok, "1"} -> false
+      _ -> true
+    end
   end
 
   @doc """
