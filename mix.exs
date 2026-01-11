305cf221b3b0b7bf36e4790d0ab54facda41ce9e
Frank Hunleth <fhunleth@troodon-software.com>
2026-01-11 12:19:56 -0500

    Fix Elixir 1.20 warning by removing unneeded code
    This code was written around 10 years ago. I initially tried refactoring
    to use functions that must not have existed back then, but then noticed
    that no warnings or errors are generated from reloading NervesBootstrap.
    
    Most users only run this code when installing Nerves. That's how I
    initially saw the warning:
    
    ```
    * Getting nerves_bootstrap (Hex package)
         warning: the variable "size" is accessed inside size(...) of a bitstring but it was defined outside of the match. You must precede it with the pin operator
         │
     117 │           <<name::binary-size(size), ".beam">> ->
         │                               ~
         │
         └─ /private/var/folders/8y/mj8t_3yx755fq60hnggw0bhr0000gn/T/mix-local-installer-fetcher-whakbQ/deps/nerves_bootstrap/mix.exs:117:31: Nerves.Bootstrap.MixProject.unload_bootstrap/1
    ```

--- mix.exs
+++ mix.exs
@@ -40,8 +40,6 @@ defmodule Nerves.Bootstrap.MixProject do
 
   def aliases do
     [
-      "compile.elixir": [&unload_bootstrap/1, "compile.elixir"],
-      run: [&unload_bootstrap/1, "run"],
       install: [
         "archive.build -o nerves_bootstrap.ez",
         "archive.install nerves_bootstrap.ez --force"
@@ -97,46 +95,5 @@ defmodule Nerves.Bootstrap.MixProject do
       flags: [:missing_return, :extra_return, :unmatched_returns, :error_handling, :underspecs],
       plt_add_apps: [:mix, :eex]
     ]
-  end
-
-  defp unload_bootstrap(_) do
-    Application.stop(:nerves_bootstrap)
-    paths = Path.wildcard(Path.join(archives_path(), "nerves_bootstrap*"))
-
-    Enum.each(paths, fn archive ->
-      ebin = archive_ebin(archive)
-      Code.delete_path(ebin)
-
-      {:ok, files} = ebin |> :unicode.characters_to_list() |> :erl_prim_loader.list_dir()
-
-      Enum.each(files, fn file ->
-        file = List.to_string(file)
-        size = byte_size(file) - byte_size(".beam")
-
-        case file do
-          <<name::binary-size(size), ".beam">> ->
-            module = String.to_atom(name)
-            :code.delete(module)
-            :code.purge(module)
-
-          _ ->
-            :ok
-        end
-      end)
-    end)
-  end
-
-  defp archives_path do
-    cond do
-      function_exported?(Mix, :path_for, 1) -> apply(Mix, :path_for, [:archives])
-      function_exported?(Mix.Local, :path_for, 1) -> apply(Mix.Local, :path_for, [:archive])
-      true -> apply(Mix.Local, :archives_path, [])
-    end
-  end
-
-  defp archive_ebin(archive) do
-    if function_exported?(Mix.Local, :archive_ebin, 1),
-      do: apply(Mix.Local, :archive_ebin, [archive]),
-      else: apply(Mix.Archive, :ebin, [archive])
   end
 end
