549deda4fb50a93f4143a232625d377ae69cfafd
Frank Hunleth <fhunleth@troodon-software.com>
2026-02-28 18:26:18 -0500

    Only check for bootstrap updates once a day
    Depending on the network speed, I'm getting check times between 100ms to
    1s. It's frequently around 500ms. To avoid paying the check penalty all
    every build, just check once a day.
    
    This works by touching a file in `~/.nerves` and then checking its
    timestamp.

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,5 @@
 # Run `mix dialyzer --format short` for strings
 [
-  {"lib/nerves_bootstrap/update_checker.ex:13:9:unknown_function Function Hex.start/0 does not exist."},
-  {"lib/nerves_bootstrap/update_checker.ex:14:45:unknown_function Function Hex.API.Package.get/2 does not exist."}
+  {"lib/nerves_bootstrap/update_checker.ex:24:9:unknown_function Function Hex.start/0 does not exist."},
+  {"lib/nerves_bootstrap/update_checker.ex:25:45:unknown_function Function Hex.API.Package.get/2 does not exist."}
 ]
