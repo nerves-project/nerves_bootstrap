b7a075cfe52266768d59a30f25197df4e9c6888b
Frank Hunleth <fhunleth@troodon-software.com>
2026-07-01 08:29:04 -0400

    Bump toolshed in fixture to fix OTP 20 warnings

--- test_fixtures/nerves_1x_otp29/mix.exs
+++ test_fixtures/nerves_1x_otp29/mix.exs
@@ -41,7 +41,7 @@ defmodule Nerves1xOtp29.MixProject do
       {:nerves, "~> 1.13", runtime: false},
       {:shoehorn, "~> 0.9.1"},
       {:ring_logger, "~> 0.11.0"},
-      {:toolshed, "~> 0.4.0"},
+      {:toolshed, "~> 0.5.0"},
 
       # Allow Nerves.Runtime on host to support development, testing and CI.
       # See config/host.exs for usage.
